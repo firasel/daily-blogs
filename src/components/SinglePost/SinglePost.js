6ea459674fd0715dd3afc980f288cb7f0b1439cf
firasel <mdrasel601390@gmail.com>
2022-03-03 15:15:55 +0600

    static page generation error solve

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -5,21 +5,21 @@ import AuthorCard from "../AuthorCard/AuthorCard";
 import Subscribe from "../Subscribe/Subscribe";
 
 const SinglePost = ({ postData }) => {
-  const { post, user } = postData;
+  const { post = {}, user = {} } = postData;
   const { data: session } = useSession({});
   return (
     <div className="lg:flex gap-7 font-[Poppins] mt-5 lg:mt-7">
       <div className="w-full">
         <div className="overflow-hidden rounded-2xl">
-          <img className="w-full" src={post.imgUrl} alt="Post image" />
+          <img className="w-full" src={post?.imgUrl} alt="Post image" />
         </div>
-        <h2 className="text-3xl font-semibold mt-5">{post.title}</h2>
+        <h2 className="text-3xl font-semibold mt-5">{post?.title}</h2>
         <p className="text-gray-700 text-base mt-1 mb-4">
-          <TimeagoReact datetime={post.updatedAt} />
+          <TimeagoReact datetime={post?.updatedAt} />
         </p>
         <div
           className="postContent prose lg:prose-xl xl:prose-2xl prose-blockquote:bg-gray-400  "
-          dangerouslySetInnerHTML={{ __html: post.content }}
+          dangerouslySetInnerHTML={{ __html: post?.content }}
         ></div>
         <div className="tags text-lg mt-4 flex flex-wrap">
           <span className="font-semibold text-xl py-2 mr-2">Tags: </span>
